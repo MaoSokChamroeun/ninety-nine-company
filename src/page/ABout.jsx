799dff730a53c59e94f14dbd1a7b7ef5a6e38a16
Mao Sokchamroeun <maosokchomroeun@gmail.com>
2025-12-14 23:35:47 +0700

    Add about new update

--- src/page/ABout.jsx
+++ src/page/ABout.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Layout from '../layout/Layout'
 import profile1 from '../assets/profile/photo_2025-10-28_13-56-56.jpg'
 import icon1 from '../assets/icon_GIF/analytics.gif'
-import icon2 from '../assets/icon_GIF/ai-image.gif'
+// import icon2 from '../assets/icon_GIF/ai-image.gif'
 import icon3 from '../assets/icon_GIF/deal.gif'
-import icon4 from '../assets/icon_GIF/solution.png' 
+import icon4 from '../assets/icon_GIF/social-care.gif' 
 import '../index.css'
 const ABout = () => {
   return (
@@ -16,41 +16,49 @@ const ABout = () => {
         data-aos-delay="300"
         data-aos-easing="ease-in-out">About</p>
 
-        <div className="our-team max-w-2xl mx-auto"
+        <div className="our-team max-w-xxl mx-auto"
         data-aos ="fade-up" 
         data-aos-duration="400"
         data-aos-delay="400"
         data-aos-easing="ease-in-out">
-          <span className="text-7xl text-center justify-center flex tracking-wide leading-17">
-            <p style={{fontWeight : 'bolder'}}>The dream team of <span className='text-pink-500'  style={{fontWeight : 'bolder'}}> degital marketing.</span></p>
+          <span className="text-sm text-center justify-center flex tracking-wide">
+            <p style={{fontWeight : "bolder" , fontSize : "20px"}}><span className='text-pink-500' style={{fontWeight : 'bolder' , fontSize : "20px"}}>Ninety Nine</span> is a creative digital agency<span className='text-pink-500' style={{fontWeight : 'bolder' , fontSize : "2px"}}>Specializing</span></p>
           </span>
-          <p className="text-center mt-4" data-aos ="fade-up" 
-        data-aos-duration="500"
-        data-aos-delay="500"
-        data-aos-easing="ease-in-out">We Grow Bussinesses Online</p>
+          <p className='text-center'>in Graphic Design, Video Editing, Advertising, and Social Media Management.</p>
         </div>
 
-        <div className="about-team max-w-7xl h-auto mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="about-team max-w-7xl mt-10 h-auto mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <div className="w-full h-full" 
             data-aos ="fade-up" 
-        data-aos-duration="550"
-        data-aos-delay="550"
-        data-aos-easing="ease-in-out">
+            data-aos-duration="550"
+            data-aos-delay="550"
+            data-aos-easing="ease-in-out">
               <img
                 src={profile1}
                 alt="Profile 1"
-                className="w-full h-full object-cover"
+                className="w-full h-full object-cover rounded-2xl"
               />
             </div>
 
                <div className="w-full h-full">
-               <p style={{fontSize : '60px'}} data-aos ="fade-up" 
+               <p style={{fontSize : '30px'}} data-aos ="fade-up" 
                 data-aos-duration="6600"
                 data-aos-delay="600"
                 data-aos-easing="ease-in-out">
-                <span className='text-pink-500 text-center'>Ninety Nine Media</span> & Advertising</p>
-                  <p className='py-20 text-gray-300' style={{fontSize : '20px'}} data-aos ="fade-up" 
+                <span className='text-pink-500 text-center' style={{fontWeight : "bolder"}}>Founder</span> Of Ninety Nine</p>
+
+                 <p className='py-1' style={{fontSize : '18px'}} data-aos ="fade-up" 
+                  data-aos-duration="600"
+                  data-aos-delay="600"
+                  data-aos-easing="ease-in-out">
+                   <span className='text-pink-500' style={{fontWeight : "bolder"}}>Ninety Nine Media & Advertising</span> គឺជាក្រុមការងារទីផ្សារឌីជីថល និងការផ្សព្វផ្សាយសម័យថ្មី ដែលបានបង្កើតឡើងនៅថ្ងៃទី ១០ ខែតុលា ឆ្នាំ ២០២៤ ដោយមានបេសកកម្មជួយអាជីវកម្មនៅក្នុងប្រទេសកម្ពុជាឲ្យអាចរីកចម្រើន ពង្រីក និងលេចធ្លោក្នុងសម័យឌីជីថលដែលមានការផ្លាស់ប្តូរយ៉ាងលឿន។
+
+                    យើងប្តេជ្ញាផ្តល់នូវដំណោះស្រាយឌីជីថលដែលមានភាពច្នៃប្រឌិត ទំនើប និងផ្តោតលើលទ្ធផល ដើម្បីជួយឲ្យម៉ាកយីហោបង្កើនអត្តសញ្ញាណរបស់ខ្លួន និងសម្រេចបាននូវជោគជ័យដែលអាចវាស់វែងបាន។
+
+                    ឈ្មោះ “Ninety Nine” តំណាងឲ្យភាពរឹងមាំ ភាពស្ថិរភាព និងការរីកចម្រើនជាបន្តបន្ទាប់ ដែលបង្ហាញពីជំនឿរបស់យើងក្នុងការផ្តល់សេវាកម្មដែលមានគុណភាពខ្ពស់ និងអាចទុកចិត្តបានទៅកាន់អតិថិជនគ្រប់រូបដែលយើងបានសហការជាមួយ។
+                    </p>
+                  <p className='py-1' style={{fontSize : '18px'}} data-aos ="fade-up" 
                   data-aos-duration="600"
                   data-aos-delay="600"
                   data-aos-easing="ease-in-out">
@@ -58,7 +66,11 @@ const ABout = () => {
                       We are committed to delivering creative, innovative, and result-driven digital solutions that help brands strengthen their identity, reach their audiences, and achieve measurable success.
                       The name “Ninety Nine” represents strength, consistency, and continuous growth—reflecting our belief in providing reliable and high-quality services to every client we work with.
                   </p>
+
+                 
             </div>
+
+            
           </div>
           
         </div>
@@ -79,7 +91,7 @@ const ABout = () => {
                             <span><img src={icon1} alt="" width={50} /></span>
                         </div>
                         <p className='text-center text-2xl'>Grow Up Business</p>
-                            <div className='text-center text-gray-600' >
+                            <div className='text-center' >
                                 We empower entrepreneurs and companies to scale with strategic 
                                 digital tools, effective campaigns, and expert guidance tailored for growth.
                             </div>
@@ -93,7 +105,7 @@ const ABout = () => {
                             <span><img src={icon4} alt="" width={50} /></span>
                         </div>
                         <p className='text-center text-2xl'>Solution for Business</p>
-                            <div className='text-center text-gray-600'>
+                            <div className='text-center'>
                             Our team delivers customized marketing solutions that address specific 
                             business challenges, helping you achieve measurable success.                            </div>
                     </div>
@@ -105,11 +117,11 @@ const ABout = () => {
                             <span><img src={icon3} alt="" width={50} /></span>
                         </div>
                         <p className='text-center text-2xl'>Long-Term Partnerships</p>
-                            <div className='text-center text-gray-600'>
+                            <div className='text-center '>
                             We build meaningful, lasting relationships with clients 
                             by continuously delivering value and evolving with your business goals.                            </div>
                     </div>
-                    <div className='max-w-xl  mx-auto p-4 rounded-2xl' data-aos ="fade-up" 
+                    {/* <div className='max-w-xl  mx-auto p-4 rounded-2xl' data-aos ="fade-up" 
                       data-aos-duration="700"
                       data-aos-delay="700"
                       data-aos-easing="ease-in-out">
@@ -120,14 +132,14 @@ const ABout = () => {
                             <div className='text-center text-gray-600'>
                             We’re committed to leading Cambodia into the digital era through innovation, 
                             education, and impactful digital marketing solutions.                            </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
 
         <div className="our-vistion w-full mt-5">
            <h1 className='text-center'><span className='text-pink-500'>Our</span> Vistion</h1>
-           <p className='text-center text-gray-400 max-w-4xl mx-auto'>
+           <p className='text-center max-w-4xl mx-auto'>
             To become one of Cambodia’s most impactful and innovative digital marketing & advertising agencies—powered by creativity, technology, and strategic thinking.
            </p>
         </div>
