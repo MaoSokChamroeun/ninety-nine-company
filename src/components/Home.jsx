8bcb35dac053c4986a5c35696eaaaa8a452943c2
Mao Sokchamroeun <maosokchomroeun@gmail.com>
2025-12-18 09:29:20 +0700

    fix react pakage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,9 @@
-
+import React from "react";
 import Layout from "../layout/Layout";
-
 import ImageSlider from "./ImageSlider";
 import WorkFlow from "./WorkFlow";
 import OurSerive from "./OurSerive";
 import OurWork from "./OurWork";
-
 import OurClient from "./OurClient";
 import BannerFooter from "./BannerFooter";
 import Action from "./Action";
