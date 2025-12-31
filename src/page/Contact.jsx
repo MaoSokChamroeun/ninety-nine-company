11b570f2be01cbc6cae2d0b41fe131cc0eb6419a
Mao Sokchamroeun <maosokchomroeun@gmail.com>
2025-12-31 14:00:31 +0700

    Update button contact

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -98,9 +98,23 @@ const Contact = () => {
                 </div>
 
                 {/* Submit button */}
-                <div className="text-right rounded-xl flex justify-end">
-                  <button className="w-[150px] bg-black h-[50px] my-3 flex items-center justify-center cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#ff0389] before:to-[rgb(255,0,174)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1]  hover:before:left-0 text-[#fff]">
-                    <Link to={'https://t.me/Mengchhayx99'} className="text-decoration-none text-white">Contact</Link>
+                <div className="text-right flex justify-end">
+                  <button class="relative inline-flex h-12 active:scale-95 transistion overflow-hidden rounded-lg p-[2px] focus:outline-none">
+                    <span class="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#e7029a_0%,#f472b6_50%,#bd5fff_100%)]"></span>
+                    <span class="inline-flex h-full w-full cursor-pointer items-center justify-center bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 undefined">
+                      <Link className="text-decoration-none text-white" to={'https://t.me/Mengchhayx99'}>Contact me</Link>
+                      <svg
+                        stroke="currentColor"
+                        fill="currentColor"
+                        stroke-width="0"
+                        viewBox="0 0 448 512"
+                        height="1em"
+                        width="1em"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M429.6 92.1c4.9-11.9 2.1-25.6-7-34.7s-22.8-11.9-34.7-7l-352 144c-14.2 5.8-22.2 20.8-19.3 35.8s16.1 25.8 31.4 25.8H224V432c0 15.3 10.8 28.4 25.8 31.4s30-5.1 35.8-19.3l144-352z"></path>
+                      </svg>
+                    </span>
                   </button>
                 </div>
               </form>
