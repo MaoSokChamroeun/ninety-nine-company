c848e878428b8694e2419ea033271ad411bdb922
Mao Sokchamroeun <maosokchomroeun@gmail.com>
2026-01-02 11:18:25 +0700

    Update color navlink

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -68,7 +68,7 @@ const Footer = () => {
 
           {/* Social Icons */}
           <div className="flex gap-3 text-3xl mx-auto md:mx-0 bg-gray-300 p-2 px-4 pe-4 rounded-2xl border-1">
-            <Link to="https://web.facebook.com/ninetyninex99" className='bg-gray-400'>
+            <Link to="https://web.facebook.com/ninetyninex99" >
            <FaFacebook className="text-[#1877F2]" />
               </Link>
 
