263218d12948d4b0d8a0de20d8d0b02575375e46
Mao Sokchamroeun <maosokchomroeun@gmail.com>
2026-05-13 16:40:45 +0700

    Update all video

--- src/data/sell.js
+++ src/data/sell.js
@@ -31,6 +31,47 @@ const sells = [
     id: "vZBStMN6_uQ",
     delay: 700,
   },
+  {
+    id : "w_sJK6VY74c",
+    delay : 300
+  },
+  {
+    id : "CyLJT09m3KM",
+    delay : 500
+  },
+  
+  {
+    id : "DAyydSEtOlI",
+    delay : 700
+  },
+  {
+    id : "63WhJJwmT3o",
+    delay : 500
+  },
+   {
+    id : "w4b8en6DE_E",
+    delay : 500
+  },
+  {
+    id : "VKZy_Ajq_rI",
+    delay : 700
+  },
+  {
+    id : "RndNsQqErmM",
+    delay : 900
+  },
+  {
+    id : "Shuc3kVg7o4",
+    delay : 500
+  },
+  {
+    id : "Uu686ly0Vsw",
+    delay : 700
+  },
+  {
+    id : "-XUdqb97L-8",
+    delay : 300
+  }
 ];
 
 export default sells;
--- src/data/videos.js
+++ src/data/videos.js
@@ -39,11 +39,51 @@ const videos = [
     delay: 700,
   },
   {
-    id : "AwIzTlEQ4LGIpl2e",
+    id : "FtbBCETNRhg",
     delay : 300
   },
   {
-    id : "lWPm7JiIGCOHFlKc",
+    id : "w_sJK6VY74c",
+    delay : 300
+  },
+  {
+    id : "CyLJT09m3KM",
+    delay : 500
+  },
+  {
+    id : "DAyydSEtOlI",
+    delay : 700
+  },
+  {
+    id : "63WhJJwmT3o",
+    delay : 500
+  },
+  {
+    id : "LWCdpNy8rck",
+    delay : 300 
+  },
+  {
+    id : "w4b8en6DE_E",
+    delay : 500
+  },
+  {
+    id : "VKZy_Ajq_rI",
+    delay : 700
+  },
+  {
+    id : "RndNsQqErmM",
+    delay : 900
+  },
+  {
+    id : "Shuc3kVg7o4",
+    delay : 500
+  },
+  {
+    id : "Uu686ly0Vsw",
+    delay : 700
+  },
+  {
+    id : "-XUdqb97L-8",
     delay : 300
   }
 ];
